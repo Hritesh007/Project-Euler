5a0b0f8e6bd89096a5cf5741b6c0220b9c7f3b0d
Hritesh kumar  Pramanik <pramanik.hritesh@gmail.com>
2019-07-31 01:00:53 +0530

    Update q3.java

--- Euler Project/src/q3.java
+++ Euler Project/src/q3.java
@@ -2,20 +2,16 @@ import java.util.Scanner;
 class q3 { 
     public static long maxPrimeFactors(long n) 
     { 
-		long max=0;
-        while (n % 2 == 0) { 
-            max=2; 
-            n /= 2; 
-        } 
-    for (long i = 3; i <= Math.sqrt(n); i += 2) { 
-            while (n % i == 0) { 
-                max=i; 
-                n /= i; 
-            } 
-        } 
-  if (n > 2) 
-            max=n; 
-		return max;
+          int i; 
+	  long x=n; 
+	  for (i=2;i<=x;i++) 
+           { 
+	       if(x%i==0){
+		  x/=i; 
+		  i--; 
+	        } 
+           } 
+	  return i;
     } 
   
     public static void main(String[] args) 
