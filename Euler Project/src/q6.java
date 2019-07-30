0c02617a8cd70f9df56b854a390434a5e38b5e40
Hritesh kumar  Pramanik <pramanik.hritesh@gmail.com>
2019-07-31 02:35:43 +0530

    Update q6.java

--- Euler Project/src/q6.java
+++ Euler Project/src/q6.java
@@ -3,16 +3,9 @@ class q6
 {
 	public static void main(String[] args) 
 	{
-		Scanner s=new Scanner(System.in);
-        System.out.println("Enter n:");
-		int n=s.nextInt();
-		int s1=0,s2=0;
-        for(int i=1;i<=n;i++)
-		{
-           s1+=Math.pow(i,2);
-		   s2+=i;
-		}
-		int res=(int)Math.pow(s2,2)-s1;
-		System.out.println("Result :"+res);
+	int n=100;
+	int sum=(n*(n+1))/2;
+	int sum_sq=((2*n+1)*(n+1)*n)/6;
+    System.out.println("Result :"+((int)Math.pow(sum,2)-sum_sq));
 	}
 }
