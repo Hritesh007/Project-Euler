db8522e28eb128d3fa082e77792137fab23187d3
Hritesh kumar  Pramanik <pramanik.hritesh@gmail.com>
2019-07-31 00:48:13 +0530

    Update q7.java

--- Euler Project/src/q7.java
+++ Euler Project/src/q7.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 class q7 
 {
-	public static boolean isPrime(int n)
-	{
-		int i;
-		for(i=2;i<=Math.sqrt(n);i++)
-		{
-			if(n%i==0)
-				return false;
-		}
-		return true;
-	}
+	public static boolean isPrime(int n) 
+    {
+		if(n==2)
+			return true;
+		if(n%2==0 && n!=2) 
+		    return false; 
+		for(int i=3;i<=Math.sqrt(n);i+=2) 
+		{ 
+                  if(n%i==0) 
+                   return false;
+                }
+	        return true;
+    }
 	public static void main(String[] args) 
 	{
 		Scanner s=new Scanner(System.in);
